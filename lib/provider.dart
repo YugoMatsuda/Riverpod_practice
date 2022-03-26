10e1357d8fe94f196c663926165a4b45266a684e
Yugo Matsuda <yugo1997zazen@gmail.com>
2022-03-26 13:44:47 +0900

    DataクラスをStateProviderにした

--- lib/provider.dart
+++ lib/provider.dart
@@ -1,7 +1,10 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'data/count_data.dart';
 
 final titleProvider = Provider<String>((ref){ return 'Riverpod Demo Home Page';});
 
 final messageProvider = Provider<String>((ref) =>  'You have pushed the button this many times:');
 
 final countProvider = StateProvider<int>((ref) => 0);
+
+final countDataProvider = StateProvider<CountData>((ref) => CountData(count: 0, countUp: 0, countDown: 0));
