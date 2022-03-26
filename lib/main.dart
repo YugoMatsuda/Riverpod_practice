c181c052f5dba564334d640eeb34a83317f31a62
Yugo Matsuda <yugo1997zazen@gmail.com>
2022-03-26 12:56:15 +0900

    ConsumerStatefulWidgetに置き換えた

--- lib/main.dart
+++ lib/main.dart
@@ -21,31 +21,37 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends ConsumerStatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends ConsumerState<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Consumer(
-            builder: (context, ref, child) => Text(ref.watch(titleProvider))),
+        title: Text(ref.watch(titleProvider)),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Consumer(builder: (context, ref, child) => Text(
-          ref.watch(messageProvider)))
-            ,
-            Consumer(builder: (context, ref, child) => Text(
-                ref.watch(countProvider).toString()))
+            Text(ref.watch(messageProvider)),
+            Text(ref.watch(countProvider).toString())
           ],
         ),
       ),
-      floatingActionButton: Consumer(builder: (context, ref, child) => FloatingActionButton(
+      floatingActionButton: FloatingActionButton(
         onPressed: () => ref.read(countProvider.state).state++,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      )),
+      ),
     );
   }
 }
+
+
+
