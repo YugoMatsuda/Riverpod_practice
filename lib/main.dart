cd433902065c3c9891c8c7304736e1a8e18b914b
Yugo Matsuda <yugo1997zazen@gmail.com>
2022-03-26 13:04:11 +0900

    MVVM用に外観を整えた

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_practice/provider.dart';
+import 'package:cupertino_icons/cupertino_icons.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -40,14 +42,35 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(ref.watch(messageProvider)),
-            Text(ref.watch(countProvider).toString())
+            Text(ref.watch(countProvider).toString()),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                FloatingActionButton(
+                  onPressed: () => ref.read(countProvider.state).state++,
+                  child: const Icon(CupertinoIcons.plus),
+                ),
+                FloatingActionButton(
+                  onPressed: () => ref.read(countProvider.state).state++,
+                  child: const Icon(CupertinoIcons.minus),
+                )
+              ],
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text('1'),
+                Text('2')
+              ],
+            )
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => ref.read(countProvider.state).state++,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.refresh),
       ),
     );
   }
